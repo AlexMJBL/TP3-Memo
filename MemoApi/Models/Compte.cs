4c7a6b7e68d02b2e984a20fc15e8d0bc6f03a0d8
abela <abela@ALEX>
2025-09-03 20:04:34 -0400

    Ajout des models pour la base de donnees

---
+++ MemoApi/Models/Compte.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemoApi.Models
+{
+    public class Compte
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string NomUtilisateur { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string MotDePasse { get; set; }
+        [Required]
+        public DateTime DateCreation { get; set; }
+        public DateTime? DateDerniereConnexion { get; set; }
+
+        public ICollection<Memo> Memos { get; set; } = new List<Memo>();
+
+
+    }
+}
---
+++ MemoApi/Models/Memo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemoApi.Models
+{
+    public class Memo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Titre { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Description { get; set; }
+        [Required]
+        public DateTime DateCreation { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string IdCompte { get; set; }
+
+        public Compte Compte { get; set; }
+
+
+    }
+}
